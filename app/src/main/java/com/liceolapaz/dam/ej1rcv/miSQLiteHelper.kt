6fea571b3f35603b784e547ae0d5632e1fc18617
Ramón Canela Villamizar <rcanelav@gmail.com>
2022-11-17 21:53:41 +0100

    UPDATE: write table method

--- app/src/main/java/com/liceolapaz/dam/ej1rcv/miSQLiteHelper.kt
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/miSQLiteHelper.kt
@@ -1,5 +1,6 @@
 package com.liceolapaz.dam.ej1rcv
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -9,5 +10,21 @@ class miSQLiteHelper(context: Context) : SQLiteOpenHelper(
     override fun onCreate(db: SQLiteDatabase?) {
         db!!.execSQL("CREATE TABLE jugadores (codigo INTEGER, nombre TEXT, precio DECIMAL, posicion TEXT, puntos INTEGER)")
     }
-    
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS jugadores")
+        onCreate(db)
+    }
+
+    fun insertPlayer(codigo: Int, nombre: String, precio: Double, posicion: String, puntos: Int) {
+        val data = ContentValues()
+        data.put("codigo", codigo)
+        data.put("nombre", nombre)
+        data.put("precio", precio)
+        data.put("posicion", posicion)
+        data.put("puntos", puntos)
+
+        val db = this.writableDatabase
+        db.insert("jugadores", null, data)
+        db.close()
+    }
 }
