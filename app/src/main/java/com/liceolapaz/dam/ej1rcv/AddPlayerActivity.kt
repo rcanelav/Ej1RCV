df215effc97e22c51e91e0a3bccafdc8678f1337
Ramón Canela Villamizar <rcanelav@gmail.com>
2022-11-17 23:10:27 +0100

    ADD: code name and price to add new Player activity

---
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/AddPlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.liceolapaz.dam.ej1rcv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_player)
+    }
+}
--- app/src/main/java/com/liceolapaz/dam/ej1rcv/PlayerListActivity.kt
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/PlayerListActivity.kt
@@ -1,8 +1,10 @@
 package com.liceolapaz.dam.ej1rcv
 
+import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.liceolapaz.dam.ej1rcv.databinding.ActivityPlayerListBinding
 
 class PlayerListActivity : AppCompatActivity() {
@@ -13,22 +15,27 @@ class PlayerListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_player_list)
         playersDBHelper = miSQLiteHelper(this)
 
-        val db : SQLiteDatabase = playersDBHelper.readableDatabase
-        val cursor = db.rawQuery("SELECT * FROM players", null)
+//        val db : SQLiteDatabase = playersDBHelper.readableDatabase
+//        val cursor = db.rawQuery("SELECT * FROM players", null)
 
-        if (cursor.moveToFirst()) {
-            do {
-                val name = cursor.getString(1)
-                val surname = cursor.getString(2)
-                val position = cursor.getString(3)
-                val number = cursor.getInt(4)
-                val team = cursor.getString(5)
-
-            } while (cursor.moveToNext())
-        }
+//        if (cursor.moveToFirst()) {
+//            do {
+//                val name = cursor.getString(1)
+//                val surname = cursor.getString(2)
+//                val position = cursor.getString(3)
+//                val number = cursor.getInt(4)
+//                val team = cursor.getString(5)
+//
+//            } while (cursor.moveToNext())
+//        }
         binding = ActivityPlayerListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val addPlayerBtn = findViewById<Button>(R.id.btnAddPlayer)
+        addPlayerBtn.setOnClickListener {
+            val intent = Intent(this, AddPlayerActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
