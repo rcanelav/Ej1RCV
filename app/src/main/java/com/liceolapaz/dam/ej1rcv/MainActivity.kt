e64d06bb79959299bc85f72b7d004bb738ac7e5a
Ramón Canela Villamizar <rcanelav@gmail.com>
2022-11-17 21:09:06 +0100

    ADD: Basic login activity

---
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.liceolapaz.dam.ej1rcv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var tries = 0;
+        val loginButton = findViewById<Button>(R.id.btnLogin);
+        loginButton.setOnClickListener({
+            val username = findViewById<EditText>(R.id.etUser).text.toString()
+            val password = findViewById<EditText>(R.id.etPassword).text.toString()
+            val errorMessage = findViewById<TextView>(R.id.txtError)
+            println("Username: $username")
+            println("Password: $password")
+
+            if (username == "admin" && password == "liceo") {
+//                val intent = Intent(this, HomeActivity::class.java)
+//                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+                print(tries)
+                errorMessage.setVisibility(View.VISIBLE)
+                if (++tries == 3) {
+                    exitProcess(0)
+                }
+            }
+        })
+    }
+}
