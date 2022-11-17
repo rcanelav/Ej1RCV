84b1c41ef8651bcb016a98ed9721aa8eec78ef7d
Ramón Canela Villamizar <rcanelav@gmail.com>
2022-11-17 22:36:39 +0100

    UPDATE: set intent in main activity

--- app/src/main/java/com/liceolapaz/dam/ej1rcv/MainActivity.kt
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.liceolapaz.dam.ej1rcv
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.text.set
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<EditText>(R.id.etUser).setText("admin")
+        findViewById<EditText>(R.id.etPassword).setText("liceo")
         var tries = 0;
         val loginButton = findViewById<Button>(R.id.btnLogin);
         loginButton.setOnClickListener{
             val username = findViewById<EditText>(R.id.etUser).text.toString()
             val password = findViewById<EditText>(R.id.etPassword).text.toString()
             val errorMessage = findViewById<TextView>(R.id.txtError)
+
             println("Username: $username")
             println("Password: $password")
 
             if (username == "admin" && password == "liceo") {
-//                val intent = Intent(this, HomeActivity::class.java)
-//                startActivity(intent)
+                val intent = Intent(this, PlayerListActivity::class.java)
+                startActivity(intent)
             } else {
                 Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
                 print(tries)
