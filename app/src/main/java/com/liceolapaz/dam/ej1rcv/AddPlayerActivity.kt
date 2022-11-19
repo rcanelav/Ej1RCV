dba15bd41db910a69934f4ebc947f0abded02c9f
Ramón Canela Villamizar <rcanelav@gmail.com>
2022-11-20 00:22:44 +0100

    ADD: position spinner and save button

--- app/src/main/java/com/liceolapaz/dam/ej1rcv/AddPlayerActivity.kt
+++ app/src/main/java/com/liceolapaz/dam/ej1rcv/AddPlayerActivity.kt
@@ -3,6 +3,8 @@ package com.liceolapaz.dam.ej1rcv
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.liceolapaz.dam.ej1rcv.databinding.ActivityAddPlayerBinding
 import com.liceolapaz.dam.ej1rcv.databinding.ActivityPlayerListBinding
 
@@ -16,5 +18,14 @@ class AddPlayerActivity : AppCompatActivity() {
 
         binding = ActivityAddPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var playerPositions = arrayOf("Portero(PT)", "Defensa(DF)", "Mediocampista(MC)", "Delantero(DL)")
+        val positionSpinner = findViewById<Spinner>(R.id.spPlayerPosition)
+        // Set the how to display it
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, playerPositions)
+        // Set the view to display when the list of choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        positionSpinner.adapter = adapter
     }
 }
